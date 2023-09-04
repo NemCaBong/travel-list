f146e4f98b00986d6e6a97cc096e7a823c3148f4
NemCaBong <89348778+NemCaBong@users.noreply.github.com>
2023-09-04 07:39:01 +0700

    Finish project

---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    // prevent reload
+    event.preventDefault();
+
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    // listen for submit form
+    // enter and click button can recognize
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😎 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* Create an array of 20 and put it in option */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item ... "
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
